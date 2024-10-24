93cb35c21993d0e596c8ec708736477e8caaf3f8
Sven Buettner <sven.buettner1@gmail.com>
2024-10-24 13:47:22 +0900

    Initial Commit

---
+++ Memory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Sven Büttner on 2024/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MemoryTests/MemoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryTests.swift
+//  MemoryTests
+//
+//  Created by Sven Büttner on 2024/10/24.
+//
+
+import Testing
+@testable import Memory
+
+struct MemoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
